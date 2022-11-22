11988832e3c5342bf2f2eab1586a507d85979025
manosanag <manosanag@ics.forth.gr>
2022-11-22 20:59:23 +0200

    Scripts to plot iostat.

---
+++ monitor_iostat.sh
@@ -0,0 +1,1 @@
+iostat-cli --output my-iostat.log --fig-output my-iostat.png monitor --iostat-args "-yxmtd -p md0 1"
---
+++ plot_iostat.sh
@@ -0,0 +1,1 @@
+iostat-cli --data my-iostat.log --disk md0 --fig-output my-iostat.png plot --title 'CS647 DB'
